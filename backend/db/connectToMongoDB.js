af47268580c17eee6a63c5554a6f842998a63fe1
Ali0316 <60759115+Ali0316@users.noreply.github.com>
2024-05-21 21:04:29 +0500

    Minor Changes

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    // await mongoose.connect(
-    //   `mongodb+srv://mralinasir40:43Uzi27wFQ40gK5c@cluster0.q8xbpac.mongodb.net/care?retryWrites=true&w=majority&appName=Cluster0`
-    // );
     await mongoose.connect(`mongodb://localhost:27017/careathome`);
     console.log("Connected to MongoDB");
   } catch (error) {
